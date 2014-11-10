7cc285268ea47298275564322ccd9bceef5edf97
ujariya <upendra.jariya@gmail.com>
2014-11-10 17:00:02 +0530

    Added skeleton classes

--- db2dbseed/src/script/create_table_framework.sql
+++ db2dbseed/src/script/create_table_framework.sql
@@ -1,12 +1,4 @@
-create table seed.SyncEntity (
-	EntityId					varchar(36)		not null,
-	EntityName					varchar(50)		not null,
-	SyncPeerLeft				varchar(36)		not null,
-	SyncPeerRight				varchar(36)		not null,
-	ProcessingOrderAddUpdate	integer			not null,
-	ProcessingOrderDelete		integer			not null,
-	primary key (EntityId)
-);
+CREATE SCHEMA seed AUTHORIZATION DBA;
 
 create table seed.SyncPeer (
 	PeerId			varchar(36)		not null,
@@ -14,9 +6,22 @@ create table seed.SyncPeer (
 	primary key (PeerId)
 );
 
+create table seed.SyncEntity (
+	EntityId					varchar(36)		not null,
+	EntityName					varchar(50)		not null,
+	SyncPeerThis				varchar(36)		not null,
+	SyncPeerRemote				varchar(36)		not null,
+	ProcessingOrderAddUpdate	integer			not null,
+	ProcessingOrderDelete		integer			not null,
+	primary key (EntityId),
+	FOREIGN KEY (SyncPeerThis) REFERENCES seed.SyncPeer(PeerId),
+	FOREIGN KEY (SyncPeerRemote) REFERENCES seed.SyncPeer(PeerId)
+);
+
 create table seed.SyncState (
 	EntityId		varchar(36)		not null,
 	RecordId		varchar(36)		not null,
 	RecordHash		varchar(100)	not null,
 	RecordData		clob			not null,
+	FOREIGN KEY (EntityId) REFERENCES seed.SyncEntity(EntityId)
 );
--- db2dbseed/src/script/create_table_model.sql
+++ db2dbseed/src/script/create_table_model.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA app AUTHORIZATION DBA;
+
 create table app.Contact (
 	ContactId			varchar(36)		not null,
 	DateOfBirth			timestamp,
@@ -10,12 +12,10 @@ create table app.Contact (
 	primary key (ContactId)
 );
 
-create table app.ContactLink (
-	ContactLinkId		varchar(36)		not null,
-	SourceContactId		varchar(36)		not null,
-	TargetContactId		varchar(36)		not null,
-	WorkHistoryId		varchar(36)		not null,
-	primary key (ContactLinkId)
+create table app.Organization (
+	OrganizationId		varchar(36)		not null,
+	Name				varchar(50)		not null,
+	primary key (OrganizationId)
 );
 
 create table app.WorkHistory (
@@ -28,5 +28,19 @@ create table app.WorkHistory (
 	EndYear				integer,
 	JobDescription		varchar(200),
 	JobTitle			varchar(50),
-	primary key (WorkHistoryId)
+	primary key (WorkHistoryId),
+	FOREIGN KEY (ContactId) REFERENCES app.Contact(ContactId),
+	FOREIGN KEY (OrganizationId) REFERENCES app.Organization(OrganizationId)
 );
+
+create table app.ContactLink (
+	ContactLinkId		varchar(36)		not null,
+	SourceContactId		varchar(36)		not null,
+	TargetContactId		varchar(36)		not null,
+	WorkHistoryId		varchar(36)		not null,
+	primary key (ContactLinkId),
+	FOREIGN KEY (SourceContactId) REFERENCES app.Contact(ContactId),
+	FOREIGN KEY (TargetContactId) REFERENCES app.Contact(ContactId),
+	FOREIGN KEY (WorkHistoryId) REFERENCES app.WorkHistory(WorkHistoryId)
+);
+
