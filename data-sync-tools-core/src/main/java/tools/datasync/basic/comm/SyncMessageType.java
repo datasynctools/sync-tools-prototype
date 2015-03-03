d1e8e36dcc60c21716efe8dbcb6816dcb691c488
doug4j <doug4j@gmail.com>
2015-03-02 21:52:43 -0500

    Improved the metrics for SyncMessageType

--- data-sync-tools-core/src/main/java/tools/datasync/basic/comm/SyncMessageType.java
+++ data-sync-tools-core/src/main/java/tools/datasync/basic/comm/SyncMessageType.java
@@ -24,107 +24,121 @@
 package tools.datasync.basic.comm;
 
 import java.io.Serializable;
-
-import tools.datasync.basic.util.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 // TODO: Need state machine pattern here...
 public class SyncMessageType implements Serializable, Cloneable {
 
-	// ------------------------------------------------
-	public static final SyncMessageType BEGIN_SYNC = new SyncMessageType("BEGIN_SYNC");
-	public static final SyncMessageType SYNC_OVER = new SyncMessageType("SYNC_OVER");
+    public static final SyncMessageType BEGIN_SYNC = new SyncMessageType(
+	    "BEGIN_SYNC");
+    public static final SyncMessageType SYNC_OVER = new SyncMessageType(
+	    "SYNC_OVER");
 
-	public static final SyncMessageType ACK = new SyncMessageType("ACK");
-	public static final SyncMessageType NACK = new SyncMessageType("NACK");
+    public static final SyncMessageType ACK = new SyncMessageType("ACK");
+    public static final SyncMessageType NACK = new SyncMessageType("NACK");
 
-	public static final SyncMessageType BEGIN_SEED = new SyncMessageType("BEGIN_SEED");
-	public static final SyncMessageType SEED = new SyncMessageType("SEED");
-	public static final SyncMessageType SEED_OVER_INIT = new SyncMessageType("SEED_OVER_INIT");
-	public static final SyncMessageType SEED_OVER_FOLLOW = new SyncMessageType("SEED_OVER_FOLLOW");
+    public static final SyncMessageType BEGIN_SEED = new SyncMessageType(
+	    "BEGIN_SEED");
+    public static final SyncMessageType SEED = new SyncMessageType("SEED");
+    public static final SyncMessageType SEED_OVER_INIT = new SyncMessageType(
+	    "SEED_OVER_INIT");
+    public static final SyncMessageType SEED_OVER_FOLLOW = new SyncMessageType(
+	    "SEED_OVER_FOLLOW");
 
-	public static final SyncMessageType ANALYSIS_STARTED = new SyncMessageType("ANALYSIS_STARTED");
-	public static final SyncMessageType ANALYSIS_OVER = new SyncMessageType("ANALYSIS_OVER");
+    public static final SyncMessageType ANALYSIS_STARTED = new SyncMessageType(
+	    "ANALYSIS_STARTED");
+    public static final SyncMessageType ANALYSIS_OVER = new SyncMessageType(
+	    "ANALYSIS_OVER");
 
-	public static final SyncMessageType CONFLICT_RESOLUTION = new SyncMessageType("CONFLICT_RESOLUTION");
+    public static final SyncMessageType CONFLICT_RESOLUTION = new SyncMessageType(
+	    "CONFLICT_RESOLUTION");
 
-	public static final SyncMessageType APPLY_CHANGES = new SyncMessageType("APPLY_CHANGES");
-	public static final SyncMessageType APPLY_CHANGES_OVER = new SyncMessageType("APPLY_CHANGES_OVER");
+    public static final SyncMessageType APPLY_CHANGES = new SyncMessageType(
+	    "APPLY_CHANGES");
+    public static final SyncMessageType APPLY_CHANGES_OVER = new SyncMessageType(
+	    "APPLY_CHANGES_OVER");
 
-	public static final SyncMessageType UNKNOWN = new SyncMessageType("UNKNOWN");
-	// ------------------------------------------------
+    public static final SyncMessageType UNKNOWN = new SyncMessageType("UNKNOWN");
 
-	private final String messageType;
-	private static final long serialVersionUID = 7007483768286153752L;
+    private final String messageType;
+    private static final long serialVersionUID = 7007483768286153752L;
+    private static List<SyncMessageType> syncMessageList = null;
 
-	protected SyncMessageType(String messageType) {
-		this.messageType = messageType;
+    protected SyncMessageType(String messageType) {
+	this.messageType = messageType;
+	if (syncMessageList != null) {
+	    syncMessageList = new ArrayList<SyncMessageType>();
+	    syncMessageList.add(BEGIN_SYNC);
+	    syncMessageList.add(SYNC_OVER);
+	    syncMessageList.add(ACK);
+	    syncMessageList.add(NACK);
+	    syncMessageList.add(BEGIN_SEED);
+	    syncMessageList.add(SEED);
+	    syncMessageList.add(SEED_OVER_INIT);
+	    syncMessageList.add(SEED_OVER_FOLLOW);
+	    syncMessageList.add(ANALYSIS_STARTED);
+	    syncMessageList.add(ANALYSIS_OVER);
+	    syncMessageList.add(CONFLICT_RESOLUTION);
+	    syncMessageList.add(APPLY_CHANGES);
+	    syncMessageList.add(APPLY_CHANGES_OVER);
+	}
+    }
+
+    public static SyncMessageType get(String message) {
+	SyncMessageType result = UNKNOWN;
+
+	if (message == null || "".equals(message)) {
+	    return result;
 	}
 
-	public static SyncMessageType get(String message) {
-		SyncMessageType result = UNKNOWN;
+	for (SyncMessageType type : syncMessageList) {
+	    if (type.messageType.equalsIgnoreCase(message)) {
+		return (type);
+	    }
+	}
 
-		if(StringUtils.isEmpty(message)){
-			return result;
-		}
-		
-		if (BEGIN_SYNC.messageType.equalsIgnoreCase(message)) {
-			result = BEGIN_SYNC;
-		} else if (SYNC_OVER.messageType.equalsIgnoreCase(message)) {
-			result = SYNC_OVER;
-		} else if (ACK.messageType.equalsIgnoreCase(message)) {
-			result = ACK;
-		} else if (NACK.messageType.equalsIgnoreCase(message)) {
-			result = NACK;
-		} else if (BEGIN_SEED.messageType.equalsIgnoreCase(message)) {
-			result = BEGIN_SEED;
-		} else if (SEED.messageType.equalsIgnoreCase(message)) {
-			result = SEED;
-		} else if (SEED_OVER_INIT.messageType.equalsIgnoreCase(message)) {
-			result = SEED_OVER_INIT;
-		} else if (SEED_OVER_FOLLOW.messageType.equalsIgnoreCase(message)) {
-			result = SEED_OVER_FOLLOW;
-		} else if (ANALYSIS_STARTED.messageType.equalsIgnoreCase(message)) {
-			result = ANALYSIS_STARTED;
-		} else if (ANALYSIS_OVER.messageType.equalsIgnoreCase(message)) {
-			result = ANALYSIS_OVER;
-		} else if (CONFLICT_RESOLUTION.messageType.equalsIgnoreCase(message)) {
-			result = CONFLICT_RESOLUTION;
-		} else if (APPLY_CHANGES.messageType.equalsIgnoreCase(message)) {
-			result = APPLY_CHANGES;
-		} else if (APPLY_CHANGES_OVER.messageType.equalsIgnoreCase(message)) {
-			result = APPLY_CHANGES_OVER;
-		} else if (UNKNOWN.messageType.equalsIgnoreCase(message)) {
-			result = UNKNOWN;
-		} else {
-			result = UNKNOWN;
-		}
-		return result;
+	return UNKNOWN;
+    }
+
+    @Override
+    public String toString() {
+	return messageType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (super.equals(obj)) {
+	    return true;
+	} else if (equalsSyncMessageType(obj)) {
+	    return true;
+	} else if (equalsString(obj)) {
+	    return true;
 	}
-	
-	@Override
-	public String toString() {
-		return messageType;
+	return false;
+    }
+
+    private boolean equalsSyncMessageType(Object obj) {
+	if (obj instanceof SyncMessageType) {
+	    if (((SyncMessageType) obj).messageType
+		    .equalsIgnoreCase(this.messageType)) {
+		return true;
+	    }
 	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if(super.equals(obj)){
-			return true;
-		}
-		if(obj instanceof SyncMessageType){
-			if(((SyncMessageType) obj).messageType.equalsIgnoreCase(this.messageType)){
-				return true;
-			}
-		} else if (obj instanceof String) {
-		    if(this.messageType.equalsIgnoreCase((String)obj)){
-		        return true;
-		    }
-		}
-		return false;
+	return false;
+    }
+
+    private boolean equalsString(Object obj) {
+	if (obj instanceof String) {
+	    if (this.messageType.equalsIgnoreCase((String) obj)) {
+		return true;
+	    }
 	}
-	
-	@Override
-	public int hashCode() {
-		return messageType.hashCode();
-	}
+	return false;
+    }
+
+    @Override
+    public int hashCode() {
+	return messageType.hashCode();
+    }
 }
