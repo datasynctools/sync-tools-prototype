06f1c1b69f5d2bc1b49db40ef078ec29b228faf9
doug4j <doug4j@gmail.com>
2015-03-22 18:13:22 -0400

    Added new Support File to Repository.

---
+++ data-sync-tools-core/src/main/java/tools/datasync/basic/sync/pump/JvmSyncPumpSenderSupport.java
@@ -0,0 +1,76 @@
+package tools.datasync.basic.sync.pump;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import tools.datasync.basic.comm.SyncMessage;
+import tools.datasync.basic.seed.SeedException;
+import tools.datasync.basic.seed.SeedOverException;
+import tools.datasync.basic.seed.SeedProducer;
+
+public class JvmSyncPumpSenderSupport {
+
+    SenderPreAckLogic senderPreAckLogic = new SenderPreAckLogic();
+    SenderPostAckLogic senderPostAckLogic = new SenderPostAckLogic();
+
+    private AtomicBoolean isRunning;
+    private AtomicBoolean stopper;
+    private long messageNumber = 0;
+
+    public void initialize(BlockingQueue<SyncMessage> sendQueue,
+	    SyncStateInitializer syncStateInitializer,
+	    JvmSyncConcurArgs concurArgs) {
+
+	this.stopper = concurArgs.getStopper();
+	this.isRunning = new AtomicBoolean(false);
+
+	senderPreAckLogic.setSendQueue(sendQueue);
+	senderPreAckLogic.setSyncStateInitializer(syncStateInitializer);
+
+	senderPostAckLogic.setIsRunning(isRunning);
+	senderPostAckLogic.setStopper(stopper);
+	senderPostAckLogic.setSendQueue(sendQueue);
+	senderPostAckLogic.setNextEntityAwaiter(concurArgs
+		.getNextEntityAwaiter());
+    }
+
+    public void runMain() throws SQLException, IOException,
+	    InterruptedException, SeedOverException, SeedException {
+
+	SenderPreAckLogicResult result = senderPreAckLogic.preAckMain(
+		isRunning, stopper, messageNumber);
+
+	if (!result.isContinueProcessing()) {
+	    return;
+	}
+
+	messageNumber = result.getMessageCount();
+
+	messageNumber = senderPostAckLogic.runPostAckMain(messageNumber);
+    }
+
+    public void stop() {
+	stopper.set(true);
+	isRunning.set(false);
+    }
+
+    public AtomicBoolean isRunning() {
+	return isRunning;
+    }
+
+    public void setAckPairReceiverLatch(CountDownLatch ackPairReceiverLatch) {
+	senderPreAckLogic.setAckPairReceiverLatch(ackPairReceiverLatch);
+    }
+
+    public void setAckPeerSenderLatch(CountDownLatch ackPeerSenderLatch) {
+	senderPreAckLogic.setAckPeerSenderLatch(ackPeerSenderLatch);
+    }
+
+    public void setSeedProducer(SeedProducer seedProducer) {
+	senderPostAckLogic.setSeedProducer(seedProducer);
+    }
+
+}
